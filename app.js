4445363498f9d5025ca967837a97214c2ec0ce84
Bikash <bikashy206@gmail.com>
2022-06-01 00:32:13 +0530

    Hashing and Salting password with bcrypt committed

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+// const md5 = require("md5");
+// const encrypt = require("mongoose-encryption");
 
 mongoose.connect("mongodb://localhost:27017/userData")
 const app = express();
@@ -14,12 +17,11 @@ const userSchema = new mongoose.Schema({
   email : String,
   password : String
 })
-
 // Encrypting the password field of our schema, to keep the user's password safe
 // Note: When we save the new document to our collections, password field will automatically get ecnrypted
 // And while searching for particular document in database using Find function encrypted field will automatically get decrypted
 // const secret = "Thisourlittlesecret";
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password'] });
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model("User", userSchema);
 app.get("/", (req, res)=>{
@@ -40,9 +42,13 @@ app.route("/login")
     }
     else{
       if(foundUser){
-        if(foundUser.password === password){
-          res.render("secrets");
-        }
+        // we are checking the hashed password in our database against the hashed password which user entered to login
+        // if matches, we let the user go ahead
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if(result){
+            res.render("secrets");
+          }
+        });
       }
     }
   });
@@ -53,18 +59,21 @@ app.route("/register")
   res.render("register");
 })
 .post((req, res)=>{
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  })
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }
-    else{
-      res.render("secrets");
-    }
-  })
+  // This will generate hash of given password with saltRounds and will return hash generated
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    })
+    newUser.save(function(err){
+      if(err){
+        console.log(err);
+      }
+      else{
+        res.render("secrets");
+      }
+    })
+  });
 });
 app.listen(3000, ()=>{
   console.log("Server started on this port.");
